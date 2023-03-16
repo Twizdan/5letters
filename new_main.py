e213ad18c5d47f24f97cb12b021519e0cd42c458
Karim Kuserbaev <krutoe8@gmail.com>
2023-03-16 21:02:42 +0300

    made find() function full indepence, also reworked coefficients

--- new_main.py
+++ new_main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import re
 
 def find(good, maybe, bad):
+    data = pd.read_csv("5letters.csv", index_col=0)["0"]
+    z = data.str.split("", expand=True).drop([0, 6], axis=1)
+    mas = [z[i + 1].value_counts(normalize=True) for i in range(5)]
     temp = data[data.str.match(good, flags=re.IGNORECASE)]
     temp = temp.str.replace(rf'[{bad} ]', '', regex=True)
     temp = temp[temp.str.len() == 5]
@@ -15,8 +18,13 @@ def find(good, maybe, bad):
         coefficients = coefficients.sum(axis=1)
         temp = pd.DataFrame(temp)
         temp[1] = coefficients
-        temp = temp.sort_values(by=1, ascending=False)
-    return temp.head(20)
+        temp = temp.sort_values(by=1, ascending=False)[:20]
+        coef_sum = temp[1].sum()
+        temp[1] = (temp[1]/coef_sum).round(3)*100
+        temp.columns = ["Слово", "Шанс (%)"]
+    if temp.shape[0] == 0:
+        return "Не найдено ни одного слова."
+    return temp.head(20).to_string(index=False)
 
 def file_creator(name: str, endname: str):
     with open(name, encoding='utf-8') as f:
@@ -30,9 +38,6 @@ def file_creator(name: str, endname: str):
         data.to_csv(endname)
 
 if __name__ == '__main__':
-    data = pd.read_csv("5letters.csv", index_col=0)["0"]
-    z = data.str.split("", expand=True).drop([0, 6], axis=1)
-    mas = [z[i + 1].value_counts(normalize=True) for i in range(5)]
     good = '1'
     while len(good) != 0:
         good = input('Напишите найденные буквы, ненайденные буквы поставьте точкой: ').lower()
