bc0850a54e6109e5dac53e6bf97bbd0feb15d7c1
Karim Kuserbaev <krutoe8@gmail.com>
2023-03-21 00:11:24 +0300

    Changed main function

--- new_main.py
+++ new_main.py
@@ -4,31 +4,33 @@ import re
 
 def find(good, maybe, bad):
     try:
-        data = pd.read_csv("5letters.csv", index_col=0)["0"]
-        z = data.str.split("", expand=True).drop([0, 6], axis=1)
-        mas = [z[i + 1].value_counts(normalize=True) for i in range(5)]
-        temp = data[data.str.match(good, flags=re.IGNORECASE)]
-        temp = temp.str.replace(rf'[{bad} ]', '', regex=True)
-        temp = temp[temp.str.len() == 5]
+        data = pd.read_csv("words_coefs.csv", index_col=0)
+        temp = data[data["0"].str.match(good, flags=re.IGNORECASE)]
+        temp["0"] = temp["0"].str.replace(rf'[{bad} ]', '', regex=True)
+        temp = temp[temp["0"].str.len() == 5]
         pattern = ('(?=.*{})' * len(maybe)).format(*maybe)
-        temp = temp[temp.str.contains(pattern)].reset_index(drop=True)
+        temp = temp[temp["0"].str.contains(pattern)].reset_index(drop=True)
         if temp.shape[0] == 0:
             return "Не найдено ни одного слова."
-        coefficients = temp.str.split("", expand=True).drop([0, 6], axis=1)
-        for i in range(5):
-            for j in range(coefficients.shape[0]):
-                coefficients[i + 1][j] = mas[i][str(coefficients[i + 1][j])]
-        coefficients = coefficients.sum(axis=1)
-        temp = pd.DataFrame(temp)
-        temp[1] = coefficients
-        temp = temp.sort_values(by=1, ascending=False)[:20]
-        coef_sum = temp[1].sum()
-        temp[1] = (temp[1] / coef_sum).round(3) * 100
-        temp.columns = ["Слово", "Шанс (%)"]
+        temp.columns = ["Слово", "Значение"]
         return temp.head(20).to_string(index=False)
     except:
         return "Вы ввели лишние символы, попробуйте ввести заново"
 
+def calculate_coefs():
+    data = pd.read_csv("words_coefs.csv", index_col=0)
+    mas = pd.read_csv("coefs.csv", index_col=0)
+    temp = data[data["0"].str.match(good, flags=re.IGNORECASE)]
+    coefficients = temp.str.split("", expand=True).drop([0, 6], axis=1)
+    for i in range(5):  # переписать
+        for j in range(coefficients.shape[0]):
+            coefficients[i + 1][j] = mas[str(i)][str(coefficients[i + 1][j])]
+    coefficients = coefficients.sum(axis=1).round(3)
+    temp = pd.DataFrame(temp)
+    temp[1] = coefficients
+    temp = temp.sort_values(by=1, ascending=False)
+    coef_sum = temp[1].sum()
+    temp[1] = (temp[1] / coef_sum).round(3) * 100
 
 def file_creator(name: str, endname: str):
     with open(name, encoding='utf-8') as f:
